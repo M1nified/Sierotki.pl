e70258b1eb2ce80594ecf45a3d4cd5f75a0aab9b
Michał Góra <Micha Gra>
2017-02-25 21:08:58 +0100

    Auto: select after paste, focus after focus

--- sierotki.main.js
+++ sierotki.main.js
@@ -1,75 +1,87 @@
 "use strict";
-(function(){
+(function () {
 
   let pasted = false;
 
-  let getInput = function(){
+  let getInput = function () {
     return document.querySelector('#input').value;
   }
-  let setInput = function(input){
+  let setInput = function (input) {
     document.querySelector('#input').value = input;
   }
-  let pasteInput = function(){
+  let pasteInput = function () {
     // document.querySelector('#input').value = '';
     // let range = document.createRange();
     // document.querySelector('#input').focus();
     // navigator.serviceWorker.controller.postMessage({execCommand:'paste'});
     // console.log(document.execCommand('paste', false, null));
   }
-  let setOutput = function(output){
+  let selectInput = function () {
+    console.log('select')
+    document.querySelector('#input').select();
+  }
+  let focusInput = function () {
+    console.log('focus')
+    document.querySelector('#input').focus();
+  }
+  let setOutput = function (output) {
     document.querySelector('#output').value = output;
   }
-  let selectOutput = function(){
+  let selectOutput = function () {
     document.querySelector('#output').select();
   }
-  let selectOutputSafe = function(){
-    if(document.querySelector('#select-after-paste').checked){
+  let selectOutputSafe = function () {
+    if (document.querySelector('#select-after-paste').checked) {
       selectOutput();
     }
   }
-  let copyOutput  = function(){
+  let copyOutput = function () {
     selectOutput();
-    ( document.execCommand('copy') && console.log('COPPIED') ) || console.log('NOT COPPIED');
+    (document.execCommand('copy') && console.log('COPPIED')) || console.log('NOT COPPIED');
   }
-  let copyOutputSafe = function(){
-    if(document.querySelector('#copy-on-paste').checked){
+  let copyOutputSafe = function () {
+    if (document.querySelector('#copy-on-paste').checked) {
       copyOutput();
     }
   }
-  let orphansFix = function(input){
+  let orphansFix = function (input) {
     return Sierotki.orphansFix(input);
   }
   let input_previous = "";
-  let interval_1 = setTimeout.bind(null,function(){
+  let interval_1 = setTimeout.bind(null, function () {
     let input = getInput();
-    if(input != input_previous){
+    if (input != input_previous) {
       setOutput(orphansFix(input));
       input_previous = input;
       // pasted && ( (pasted=false) || copyOutputSafe() );
-      pasted && ( (pasted=false) || selectOutputSafe() );
+      pasted && ((pasted = false) || selectOutputSafe());
     }
     interval_1();
-  },100);
-  window.addEventListener('load',function(){
+  }, 100);
+  window.addEventListener('load', function () {
     interval_1();
     // document.designMode="On";
     if (navigator.serviceWorker) {
-      navigator.serviceWorker.register('/sierotki.background.js').then(function(registration) {
-        console.log('ServiceWorker registration successful with scope:',  registration.scope);
-      }).catch(function(error) {
+      navigator.serviceWorker.register('/sierotki.background.js').then(function (registration) {
+        console.log('ServiceWorker registration successful with scope:', registration.scope);
+      }).catch(function (error) {
         console.log('ServiceWorker registration failed:', error);
       });
     }
-    document.querySelector('#input').addEventListener('paste',function(){
+    document.querySelector('#input').addEventListener('paste', function () {
       console.log('paste');
       pasted = true;
     })
-    document.querySelector('#output').addEventListener('dblclick',function(){
+    document.querySelector('#output').addEventListener('dblclick', function () {
       copyOutput();
     })
     // document.querySelector('#input').addEventListener('click',function(){
     //   console.log('click');
     //   pasteInput();
     // })
+    window.addEventListener('focus', function (event) {
+      event.preventDefault();
+      if (document.querySelector('#focus-after-focus').checked) selectInput();
+    });
   });
 })();
